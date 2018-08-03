13c813763d9931968999abf65b162b5521911533
Michael <michael.green94@hotmail.com>
2018-08-02 21:22:30 -0600

    finished

--- arrays.js
+++ arrays.js
@@ -32,6 +32,21 @@ var tom = {
 	title: 'instructor'
 };
 
+contacts.push(jake, matt, tony, andrew, tom);
+console.log(contacts)
+console.clear()
 
-//3. Woops after adding all of those people to the same contacts list you realized you need a list just the instructors. Create a new variable named instructors populate it using contacts array. 
+//3. Woops after adding all of those people to the same contacts list you realized you need a list of just the instructors. Create a new variable named instructors populate it using contacts array. 
 
+let instructors = [];
+
+function findInstructors(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		const attribute = arr[i];
+		if (attribute.title == 'instructor') {
+			instructors.push(attribute.name)
+		}
+	}
+}
+findInstructors(contacts)
+console.log(instructors)
